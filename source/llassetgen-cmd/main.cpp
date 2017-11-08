146c811e88fd8b95073b326a97ef54169ee5cc63
Alexander Meißner <AlexanderMeissner@gmx.net>
2017-11-08 07:11:10 +0100

    Added dependencies: freetype and png

--- source/llassetgen-cmd/main.cpp
+++ source/llassetgen-cmd/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #include <llassetgen/llassetgen.h>
-
+using namespace llassetgen;
 
 int main(int argc, char** argv) {
 
--- source/llassetgen/source/llassetgen.cpp
+++ source/llassetgen/source/llassetgen.cpp
@@ -1,3 +1,7 @@
+#include <ft2build.h>
+#include <freetype/freetype.h>
+#include <png.h>
+
 #include <llassetgen/llassetgen.h>
 
 namespace llassetgen {
