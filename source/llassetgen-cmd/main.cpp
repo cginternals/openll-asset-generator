26cff0809a35ab96472fb57ada2b4de8692950d4
Justin Trautmann <justin.trautmann@student.hpi.de>
2017-11-23 13:44:21 +0100

    revert main.cpp changes

--- source/llassetgen-cmd/main.cpp
+++ source/llassetgen-cmd/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char** argv) {
     std::unique_ptr<DistanceTransform> dt(new DeadReckoning());
     if(argc == 5 && strcmp(argv[1], "-dt-from-glyph") == 0) {
         // Example: llassetgen-cmd -dt-from-glyph G /Library/Fonts/Verdana.ttf glyph.png
-		FT_Face face;
+        FT_Face face;
         assert(FT_New_Face(freetype, argv[3], 0, &face) == 0);
         assert(FT_Set_Pixel_Sizes(face, 0, 128) == 0);
         assert(FT_Load_Glyph(face, FT_Get_Char_Index(face, argv[2][0]), FT_LOAD_RENDER | FT_LOAD_TARGET_MONO) == 0);
