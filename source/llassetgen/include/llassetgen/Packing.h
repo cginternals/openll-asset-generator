b0df48fde67063a750384ba65f0fc143b3dc7731
David Stangl <david.stangl@student.hpi.de>
2018-01-09 23:55:34 +0100

    Fix symbol visibility

--- source/llassetgen/include/llassetgen/Packing.h
+++ source/llassetgen/include/llassetgen/Packing.h
@@ -24,7 +24,7 @@ namespace llassetgen {
 
         Packing() = default;
 
-        explicit Packing(Vec2<PackingSizeType> _atlasSize);
+        LLASSETGEN_API explicit Packing(Vec2<PackingSizeType> _atlasSize);
     };
 
     namespace impl_packing {
