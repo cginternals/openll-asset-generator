f63680cfb6c9ea16cd4763e51239bd9936fad11f
anne.gropler <anne.gropler@student.hpi.uni-potsdam.de>
2018-01-10 14:39:48 +0100

    created app that uses Qt to display image

---
+++ source/llassetgen-rendering/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include <ft2build.h> // NOLINT include order required by freetype
+#include FT_FREETYPE_H
+
+#include <QApplication>
+#include <QLabel>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+
+#include <llassetgen/llassetgen.h>
+
+int main(int argc, char** argv) {
+    llassetgen::init();
+
+    QApplication a(argc, argv);
+
+    QString path(a.applicationDirPath());
+    auto bla = path.toStdString();
+    QString fileName(path + "/../../data/arial_regular_300_512.png");
+    bla = fileName.toStdString();
+    QGraphicsScene scene;
+    scene.addPixmap(QPixmap(fileName));
+    QGraphicsView view(&scene);
+    view.show();
+    
+    return a.exec();
+}
