e5334408d29b08723b36949e148df9ed069003b6
Alexander Meißner <AlexanderMeissner@gmx.net>
2018-02-22 14:50:27 +0100

    Added example usage of ported DistanceTransform

--- source/tests/llassetgen-tests/llassetgen-tests.cpp
+++ source/tests/llassetgen-tests/llassetgen-tests.cpp
@@ -1,10 +1,24 @@
 #include <gmock/gmock.h>
 #include <llassetgen/llassetgen.h>
 
+using namespace llassetgen;
+
 class llassetgen_tests : public testing::Test {
 
 };
 
-TEST_F(llassetgen_tests, CheckSomeResults) {
+/*TEST_F(llassetgen_tests, DeadReckoning) {
+    Image input("input.png"), output(input.get_width(), input.get_height(), sizeof(DistanceTransform::OutputType)*8);
+    std::unique_ptr<DistanceTransform> dt(new DeadReckoning(input, output));
+    dt->transform();
+    output.exportPng<DistanceTransform::OutputType>("DeadReckoning.png", -20, 50);
     EXPECT_EQ(1, 1);
 }
+
+TEST_F(llassetgen_tests, ParabolaEnvelope) {
+    Image input("input.png"), output(input.get_width(), input.get_height(), sizeof(DistanceTransform::OutputType)*8);
+    std::unique_ptr<DistanceTransform> dt(new ParabolaEnvelope(input, output));
+    dt->transform();
+    output.exportPng<DistanceTransform::OutputType>("ParabolaEnvelope.png", -20, 50);
+    EXPECT_EQ(1, 1);
+}*/
