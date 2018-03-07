490fe631b20e4ded95fd4844f0bfd06a433fabb8
Lukas Behrendt <lukas.behrendt@student.hpi.de>
2018-03-07 16:40:58 +0100

    clang-format

--- source/llassetgen-cmd/main.cpp
+++ source/llassetgen-cmd/main.cpp
@@ -2,7 +2,7 @@
 #include <map>
 
 #include <CLI11.h>
-#include <ft2build.h> // NOLINT include order required by freetype
+#include <ft2build.h>  // NOLINT include order required by freetype
 #include FT_FREETYPE_H
 
 #include <llassetgen/llassetgen.h>
@@ -10,16 +10,18 @@
 
 using namespace llassetgen;
 
-std::map<std::string, std::function<std::unique_ptr<DistanceTransform>(Image&, Image&)>> dtFactory {
-    std::make_pair("deadrec", [](Image& input, Image& output){
-        return std::unique_ptr<DistanceTransform>(new DeadReckoning(input, output));
-    }),
-    std::make_pair("parabola", [](Image& input, Image& output){
-        return std::unique_ptr<DistanceTransform>(new ParabolaEnvelope(input, output));
-    }),
+std::map<std::string, std::function<std::unique_ptr<DistanceTransform>(Image&, Image&)>> dtFactory{
+    std::make_pair("deadrec",
+                   [](Image& input, Image& output) {
+                       return std::unique_ptr<DistanceTransform>(new DeadReckoning(input, output));
+                   }),
+    std::make_pair("parabola",
+                   [](Image& input, Image& output) {
+                       return std::unique_ptr<DistanceTransform>(new ParabolaEnvelope(input, output));
+                   }),
 };
 
-std::unique_ptr<Image> loadGlyph(FT_ULong glyph, std::string &font_path) {
+std::unique_ptr<Image> loadGlyph(FT_ULong glyph, std::string& font_path) {
     FT_Face face;
     assert(FT_New_Face(freetype, font_path.c_str(), 0, &face) == 0);
     assert(FT_Set_Pixel_Sizes(face, 0, 128) == 0);
@@ -28,8 +30,8 @@ std::unique_ptr<Image> loadGlyph(FT_ULong glyph, std::string &font_path) {
     return std::unique_ptr<Image>(new Image(*bitmap));
 }
 
-void distField(std::string &algorithm, Image &input, std::string &out_path) {
-    Image output = Image(input.getWidth(), input.getHeight(), sizeof(DistanceTransform::OutputType)*8);
+void distField(std::string& algorithm, Image& input, std::string& out_path) {
+    Image output = Image(input.getWidth(), input.getHeight(), sizeof(DistanceTransform::OutputType) * 8);
     std::unique_ptr<DistanceTransform> dt = dtFactory[algorithm](input, output);
     dt->transform();
     output.exportPng<DistanceTransform::OutputType>(out_path, -20, 50);
@@ -44,27 +46,27 @@ int main(int argc, char** argv) {
     llassetgen::init();
 
     CLI::App app{"OpenLL Font Asset Generator"};
-    CLI::App *distfield = app.add_subcommand("distfield");
-    CLI::App *atlas = app.add_subcommand("atlas");
+    CLI::App* distfield = app.add_subcommand("distfield");
+    CLI::App* atlas = app.add_subcommand("atlas");
 
     // distfield params
     std::string algorithm = "deadrec";  // default value
     std::set<std::string> algo_options;
-    for(auto& algo : dtFactory) {
+    for (auto& algo : dtFactory) {
         algo_options.insert(algo.first);
     }
     distfield->add_set("-a,--algorithm", algorithm, algo_options);
 
     std::string glyph;
-    CLI::Option *glyph_opt = distfield->add_option("-g,--glyph", glyph);
+    CLI::Option* glyph_opt = distfield->add_option("-g,--glyph", glyph);
 
     std::string font_path;
-    CLI::Option * font_opt = distfield->add_option("--font,-f", font_path)->check(CLI::ExistingFile);
+    CLI::Option* font_opt = distfield->add_option("--font,-f", font_path)->check(CLI::ExistingFile);
     font_opt->requires(glyph_opt);
     glyph_opt->requires(font_opt);
 
     std::string img_path;
-    CLI::Option *img_opt = distfield->add_option("--image,-i", img_path)->check(CLI::ExistingFile);
+    CLI::Option* img_opt = distfield->add_option("--image,-i", img_path)->check(CLI::ExistingFile);
     img_opt->excludes(glyph_opt);
 
     std::string out_path;
@@ -75,7 +77,7 @@ int main(int argc, char** argv) {
 
     CLI11_PARSE(app, argc, argv);
 
-    if(app.got_subcommand(distfield)) {
+    if (app.got_subcommand(distfield)) {
         std::unique_ptr<Image> input;
         if (glyph_opt->count()) {
             // Example: llassetgen-cmd distfield -a parabola -g G --font="/Library/Fonts/Verdana.ttf" glyph.png
@@ -91,10 +93,10 @@ int main(int argc, char** argv) {
         }
 
         distField(algorithm, *input, out_path);
-    } else if(app.got_subcommand(atlas)) {
+    } else if (app.got_subcommand(atlas)) {
         // TODO
     } else {
-       return app.exit(CLI::CallForHelp());
+        return app.exit(CLI::CallForHelp());
     }
 
     return 0;
