e0fb4c1ffe4101620fbc6c60259da2272a9b22db
Lukas Behrendt <lukas.behrendt@student.hpi.de>
2018-03-26 22:16:05 +0200

    Refactor subcommand parsing into separate functions

--- source/llassetgen-cmd/source/main.cpp
+++ source/llassetgen-cmd/source/main.cpp
@@ -8,16 +8,28 @@
 
 using namespace llassetgen;
 
-std::map<std::string, std::function<std::unique_ptr<DistanceTransform>(Image&, Image&)>> dtFactory{
-    std::make_pair("deadrec",
-                   [](Image& input, Image& output) {
-                       return std::unique_ptr<DistanceTransform>(new DeadReckoning(input, output));
-                   }),
-    std::make_pair("parabola",
-                   [](Image& input, Image& output) {
-                       return std::unique_ptr<DistanceTransform>(new ParabolaEnvelope(input, output));
-                   }),
+std::map<std::string, std::function<std::unique_ptr<DistanceTransform>(Image&, Image&)>> dtFactory {
+    {
+        "deadrec",
+        [](Image &input, Image &output) {
+            return std::unique_ptr<DistanceTransform>(new DeadReckoning(input, output));
+        }
+    },
+    {
+        "parabola",
+        [](Image& input, Image& output) {
+            return std::unique_ptr<DistanceTransform>(new ParabolaEnvelope(input, output));
+        }
+    }
 };
+
+std::set<std::string> algoOptions() {
+    std::set<std::string> options;
+    for (const auto& algo : dtFactory) {
+        options.insert(algo.first);
+    }
+    return options;
+}
 
 std::u32string UTF8toUCS4(const std::string& str) {
 #if _MSC_VER >= 1900  // handle Visual Studio bug
@@ -30,13 +42,6 @@ std::u32string UTF8toUCS4(const std::string& str) {
 #endif
 }
 
-void distField(std::string& algorithm, Image& input, std::string& outPath) {
-    Image output = Image(input.getWidth(), input.getHeight(), sizeof(DistanceTransform::OutputType) * 8);
-    std::unique_ptr<DistanceTransform> dt = dtFactory[algorithm](input, output);
-    dt->transform();
-    output.exportPng<DistanceTransform::OutputType>(outPath, 20, -30);
-}
-
 std::vector<Vec2<size_t>> sizes(const std::vector<Image>& images, size_t padding = 0) {
     std::vector<Vec2<size_t>> imageSizes(images.size());
     std::transform(images.begin(), images.end(), imageSizes.begin(),
@@ -45,86 +50,93 @@ std::vector<Vec2<size_t>> sizes(const std::vector<Image>& images, size_t padding
     return imageSizes;
 };
 
-int main(int argc, char** argv) {
-    llassetgen::init();
-
+int parseAtlas(int argc, char **argv) {
     CLI::App app{"OpenLL Font Asset Generator"};
-    CLI::App* distfield = app.add_subcommand("distfield");
-    CLI::App* atlas = app.add_subcommand("atlas");
 
-    // distfield params
     std::string algorithm = "deadrec";  // default value
-    std::set<std::string> algoOptions;
-    for (const auto& algo : dtFactory) {
-        algoOptions.insert(algo.first);
-    }
-    distfield->add_set("-a,--algorithm", algorithm, algoOptions);
+    CLI::Option* distfieldOpt = app.add_set("-d,--distfield", algorithm, algoOptions());
 
     std::string glyphs;
-    CLI::Option* glyphsOpt = distfield->add_option("-g,--glyph", glyphs);
+    CLI::Option* glyphsOpt = app.add_option("-g,--glyph", glyphs);
 
     std::string fontPath;
-    CLI::Option* fontPathOpt = distfield->add_option("--fontpath", fontPath)->check(CLI::ExistingFile);
-    fontPathOpt->requires(glyphsOpt);
+    CLI::Option* fontPathOpt = app.add_option("--fontpath", fontPath)->check(CLI::ExistingFile);
 
     std::string fontName;
-    CLI::Option* fontNameOpt = distfield->add_option("--fontname,-f", fontName);
-    fontNameOpt->requires(glyphsOpt);
+    CLI::Option* fontNameOpt = app.add_option("--fontname,-f", fontName);
 
     glyphsOpt->requires_one({fontPathOpt, fontNameOpt});
 
     int fontSize = 128;
-    distfield->add_option("-s,--fontsize", fontSize)->requires(glyphsOpt);
-
-    std::string imgPath;
-    CLI::Option* imgOpt = distfield->add_option("--image,-i", imgPath)->check(CLI::ExistingFile);
-    imgOpt->excludes(glyphsOpt);
+    app.add_option("-s,--fontsize", fontSize);
 
     std::string outPath;
-    distfield->add_option("outfile", outPath)->required();
-
-    // atlas params
-    // TODO
+    app.add_option("outfile", outPath)->required();
 
     CLI11_PARSE(app, argc, argv);
 
-    if (app.got_subcommand(distfield)) {
-        std::unique_ptr<Image> input;
-        if (glyphsOpt->count()) {
-            // Example: llassetgen-cmd distfield -a parabola -g G -f Verdana -s 64 glyph.png
-            try {
-                FontFinder fontFinder = fontPathOpt->count() ? FontFinder::fromPath(fontPath)
-                                                             : FontFinder::fromName(fontName);
+    // Example: llassetgen-cmd atlas -d parabola -g ABC -f Verdana -s 64 atlas.png
+    try {
+        FontFinder fontFinder = fontPathOpt->count() ? FontFinder::fromPath(fontPath)
+                                                     : FontFinder::fromName(fontName);
 
-                auto ucs4Glyphs = UTF8toUCS4(glyphs);
-                size_t padding = 20;
-                std::vector<Image> glyphImages;
-                fontFinder.renderGlyphs(ucs4Glyphs, glyphImages, fontSize, padding);
-                std::vector<Vec2<size_t>> imageSizes = sizes(glyphImages);
-                Packing p = shelfPackAtlas(imageSizes.begin(), imageSizes.end(), false);
-                Image atlas = distanceFieldAtlas<ParabolaEnvelope>(glyphImages.begin(), glyphImages.end(), p);
-                atlas.exportPng<DistanceTransform::OutputType>(outPath, 20, -30);
-                return 0;
-            } catch (const std::exception& e) {
-                std::cerr << "Error: " << e.what() << std::endl;
-                return 2;
-            }
+        auto ucs4Glyphs = UTF8toUCS4(glyphs);
+        size_t padding = 20;
+        std::vector<Image> glyphImages;
+        fontFinder.renderGlyphs(ucs4Glyphs, glyphImages, fontSize, padding);
+        std::vector<Vec2<size_t>> imageSizes = sizes(glyphImages);
+        Packing p = shelfPackAtlas(imageSizes.begin(), imageSizes.end(), false);
 
-        } else if (imgOpt->count()) {
-            // Example: llassetgen-cmd distfield -a deadrec -i input.png output.png
-            input = std::unique_ptr<Image>(new Image(imgPath));
-        } else {
-            return distfield->exit(CLI::CallForHelp());
+        if (distfieldOpt->count()) {
+            Image atlas = distanceFieldAtlas<ParabolaEnvelope>(glyphImages.begin(), glyphImages.end(), p);
+            atlas.exportPng<DistanceTransform::OutputType>(outPath, 20, -30);
         }
-
-        if (!input) return 2;
-
-        distField(algorithm, *input, outPath);
-    } else if (app.got_subcommand(atlas)) {
-        // TODO
-    } else {
-        return app.exit(CLI::CallForHelp());
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 2;
     }
 
     return 0;
+}
+
+int parseDistField(int argc, char** argv) {
+    CLI::App app{"OpenLL Font Asset Generator"};
+    // Example: llassetgen-cmd distfield -a deadrec -i input.png output.png
+
+    std::string algorithm = "deadrec";  // default value
+    app.add_set("-a,--algorithm", algorithm, algoOptions());
+
+    std::string imgPath;
+    app.add_option("--image,-i", imgPath)->required()->check(CLI::ExistingFile);
+
+    std::string outPath;
+    app.add_option("outfile", outPath)->required();
+
+    CLI11_PARSE(app, argc, argv);
+
+    Image input = Image(imgPath);
+    Image output = Image(input.getWidth(), input.getHeight(), sizeof(DistanceTransform::OutputType) * 8);
+    std::unique_ptr<DistanceTransform> dt = dtFactory[algorithm](input, output);
+    dt->transform();
+    output.exportPng<DistanceTransform::OutputType>(outPath, 20, -30);
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    llassetgen::init();
+
+    CLI::App app{"OpenLL Font Asset Generator"};
+    CLI::App* atlas = app.add_subcommand("atlas")->allow_extras();
+    CLI::App* distfield = app.add_subcommand("distfield")->allow_extras();
+
+    CLI11_PARSE(app, argc, argv);
+    --argc;
+    ++argv;
+
+    if (app.got_subcommand(atlas)) {
+        return parseAtlas(argc, argv);
+    } else if (app.got_subcommand(distfield)) {
+        return parseDistField(argc, argv);
+    }
+    return app.exit(CLI::CallForHelp());
 }
