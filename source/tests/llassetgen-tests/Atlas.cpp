2e01f406e0078d8054ef26037e28aa0cf688c5b8
Lukas Behrendt <lukas.behrendt@student.hpi.de>
2018-03-26 22:28:34 +0200

    'Fix' tests

--- source/tests/llassetgen-tests/Atlas.cpp
+++ source/tests/llassetgen-tests/Atlas.cpp
@@ -8,6 +8,7 @@ std::vector<Vec2<size_t>> atlasTestSizes {{1, 1}, {34, 5}, {23, 79}, {16, 70}, {
 
 TEST(AtlasTest, CreateDistanceFieldAtlas) {
     std::vector<Image> glyphs;
+    glyphs.reserve(atlasTestSizes.size());
 
     for (const auto& size : atlasTestSizes) {
         glyphs.emplace_back(size.x, size.y, 1);
@@ -22,6 +23,7 @@ TEST(AtlasTest, CreateDistanceFieldAtlas) {
 
 TEST(AtlasTest, CreateFontAtlas) {
     std::vector<Image> glyphs;
+    glyphs.reserve(atlasTestSizes.size());
 
     for (const auto& size : atlasTestSizes) {
         glyphs.emplace_back(size.x, size.y, 1);
