0db2130ac566be5d220ec33296ac905af444922e
Lukas Behrendt <lukas.behrendt@student.hpi.de>
2018-03-30 13:52:30 +0200

    Remove using namespace from header

--- source/llassetgen-cmd/include/FontFinder.h
+++ source/llassetgen-cmd/include/FontFinder.h
@@ -9,27 +9,27 @@
 
 #include <llassetgen/llassetgen.h>
 
-using namespace llassetgen;
+namespace llassetgen {
+    class FontFinder {
+       public:
+        static FontFinder fromName(const std::string &fontName);
+        static FontFinder fromPath(const std::string &fontPath);
 
-class FontFinder {
-   public:
-    static FontFinder fromName(const std::string& fontName);
-    static FontFinder fromPath(const std::string& fontPath);
+        Image renderGlyph(unsigned long glyph, int size, size_t padding = 0);
+        void renderGlyphs(const std::u32string &glyphs, std::vector<Image> &v, int size, size_t padding = 0);
 
-    Image renderGlyph(unsigned long glyph, int size, size_t padding = 0);
-    void renderGlyphs(const std::u32string& glyphs, std::vector<Image>& v, int size, size_t padding = 0);
-
-   private:
-    FontFinder() = default;
+       private:
+        FontFinder() = default;
 
 #ifdef _WIN32
-    bool getFontData(const std::string& fontName);
+        bool getFontData(const std::string& fontName);
 #elif __unix__
-    static bool findFontPath(const std::string& fontName, std::string& fontPath);
+        static bool findFontPath(const std::string &fontName, std::string &fontPath);
 #endif
 
-    FT_Face fontFace;
+        FT_Face fontFace;
 #ifdef _WIN32
-    std::vector<FT_Byte> fontData;
+        std::vector<FT_Byte> fontData;
 #endif
-};
+    };
+}
