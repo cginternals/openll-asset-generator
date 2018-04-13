0328681b2e5a5ef0eb8c3682143ecdb09da60638
anne.gropler <anne.gropler@student.hpi.uni-potsdam.de>
2018-04-13 17:33:10 +0200

    cpack files for windows installer; still doesn't copy the dlls

---
+++ cmake/cpack-windows-dlls.cmake
@@ -0,0 +1,39 @@
+find_file(DLLS_GLBINDING glbinding.dll)
+find_file(DLLS_GLOBJECTS globjects.dll)
+find_file(DLLS_LIBPNG libpng16.dll)
+find_file(DLLS_ZLIB zlib.dll)
+find_file(DLLS_QT5CORE qt5core.dll)
+find_file(DLLS_QT5GUI qt5gui.dll)
+find_file(DLLS_QT5WIDGETS qt5widgets.dll)
+find_file(DLLS_MSVCRT msvcrt.dll)
+find_file(DLLS_MSVCP114 msvcp140.dll) 
+find_file(DLLS_MSVCR114 msvcr140.dll)
+find_file(DLLS_VCRUNTIME vcruntime140.dll)
+
+
+
+
+set(OPTION_DEPLOY_DEBUG_DLLS FALSE CACHE BOOL "if set, debug versions of certain dlls will be packaged")
+if (OPTION_DEPLOY_DEBUG_DLLS)
+	find_file(DLLS_MSVCP114D msvcp114d.dll)
+	find_file(DLLS_MSVCR114D msvcr114d.dll)
+else()
+endif()
+
+set(DLLS
+    ${DLLS_GLBINDING}
+    ${DLLS_GLOBJECTS}
+    ${DLLS_LIBPNG}
+    ${DLLS_ZLIB}
+	${DLLS_QT5CORE}
+	${DLLS_QT5GUI}
+	${DLLS_QT5WIDGETS}
+	${DLLS_MSVCRT}	
+	${DLLS_MSVCP114}
+	${DLLS_MSVCR114}
+	${DLLS_MSVCP114D}
+	${DLLS_MSVCR114D}
+    ${DLLS_VCRUNTIME}
+)
+
+install(FILES ${DLLS} DESTINATION .) ###wichtigste zeile, install bin ist "."
